ce733145bf00b12b86fb7e14d8c8c6af1e60868c
Stéphane <s.bressani@bluewin.ch>
2016-11-26 14:36:58 +0100

    ajout git

---
+++ TrimWav/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TrimWav
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nbr_partie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nom_fichier = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Trim = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output Dir";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(91, 34);
+            this.input.Margin = new System.Windows.Forms.Padding(4);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(719, 22);
+            this.input.TabIndex = 2;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(91, 65);
+            this.output.Margin = new System.Windows.Forms.Padding(4);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(719, 22);
+            this.output.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(395, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre de parties (80 dans renoise (1) = 1:4 / 2:4 / 3:4 / 4:4)";
+            // 
+            // nbr_partie
+            // 
+            this.nbr_partie.Location = new System.Drawing.Point(420, 96);
+            this.nbr_partie.Margin = new System.Windows.Forms.Padding(4);
+            this.nbr_partie.Name = "nbr_partie";
+            this.nbr_partie.Size = new System.Drawing.Size(37, 22);
+            this.nbr_partie.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Format pour SPS 24bit / 44100Hz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(467, 100);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nom fichier";
+            // 
+            // nom_fichier
+            // 
+            this.nom_fichier.Location = new System.Drawing.Point(555, 96);
+            this.nom_fichier.Margin = new System.Windows.Forms.Padding(4);
+            this.nom_fichier.Name = "nom_fichier";
+            this.nom_fichier.Size = new System.Drawing.Size(191, 22);
+            this.nom_fichier.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(755, 100);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "N°.wav";
+            // 
+            // Trim
+            // 
+            this.Trim.Location = new System.Drawing.Point(711, 128);
+            this.Trim.Margin = new System.Windows.Forms.Padding(4);
+            this.Trim.Name = "Trim";
+            this.Trim.Size = new System.Drawing.Size(100, 28);
+            this.Trim.TabIndex = 11;
+            this.Trim.Text = "Trim";
+            this.Trim.UseVisualStyleBackColor = true;
+            this.Trim.Click += new System.EventHandler(this.Trim_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(20, 164);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(789, 164);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 143);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Log";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(380, 11);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(273, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Control > Memory > Bitrate dans octatrack";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 343);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Trim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nom_fichier);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nbr_partie);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Trim Wav File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nbr_partie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nom_fichier;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Trim;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ TrimWav/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace TrimWav
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            input.Text = "c:\\MP3\\import_export_csharp\\import\\essai_24_44100_800renoise_16.wav";
+            output.Text = "C:\\MP3\\import_export_csharp\\export\\";
+            nbr_partie.Text = "16";
+            nom_fichier.Text = "export";
+        }
+
+        private void Trim_Click(object sender, EventArgs e)
+        {
+            // Lecture de l'input
+            WaveFileReader reader = new WaveFileReader(input.Text.ToString());
+            TimeSpan span = reader.TotalTime;
+            long span_total_long = span.Ticks;
+            long nbr_partie_long = Int64.Parse(nbr_partie.Text.ToString());
+            long variable_saut_wav_long = span_total_long / nbr_partie_long;
+
+            // Variable de saut en TimeSpan
+            TimeSpan variable_saut_wav_timespan = TimeSpan.FromTicks(variable_saut_wav_long);
+
+            // boucle de découpe du fichier wav
+            for (int i = 0; i < nbr_partie_long; i++)
+            {
+                string output_string = output.Text.ToString() + i.ToString() + nom_fichier.Text.ToString() + ".wav";
+                if (i == nbr_partie_long - 1)
+                {
+                    WavFileUtils.TrimWavFile(input.Text.ToString(), output_string, TimeSpan.FromTicks(variable_saut_wav_long * i), TimeSpan.FromTicks((variable_saut_wav_long * i) + variable_saut_wav_long));
+                    listBox1.Items.Add(i.ToString() + " --- " + TimeSpan.FromTicks(variable_saut_wav_long * i).ToString() + " --- " + TimeSpan.FromTicks((variable_saut_wav_long * i) + variable_saut_wav_long).ToString());
+                }
+                else
+                {
+                    WavFileUtils.TrimWavFile(input.Text.ToString(), output_string, TimeSpan.FromTicks(variable_saut_wav_long * i), TimeSpan.FromTicks((variable_saut_wav_long * i) + variable_saut_wav_long - 1));
+                    listBox1.Items.Add(i.ToString() + " --- " + TimeSpan.FromTicks(variable_saut_wav_long * i).ToString() + " --- " + TimeSpan.FromTicks((variable_saut_wav_long * i) + variable_saut_wav_long - 1).ToString());
+                }
+            }
+            Form.ActiveForm.Refresh();
+        }
+    }
+}
+    public static class WavFileUtils
+    {
+        public static void TrimWavFile(string inPath, string outPath, TimeSpan cutFromStart, TimeSpan cutFromEnd)
+        {
+            using (WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using (WaveFileWriter writer = new WaveFileWriter(outPath, reader.WaveFormat))
+                {
+                    int bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond / 1000;
+
+                    int startPos = (int)cutFromStart.TotalMilliseconds * bytesPerMillisecond;
+                    startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+
+                    int endBytes = (int)cutFromEnd.TotalMilliseconds * bytesPerMillisecond;
+                    endBytes = endBytes - endBytes % reader.WaveFormat.BlockAlign;
+                    //int endPos = (int)reader.Length - endBytes;
+
+                    int endPos = endBytes;
+
+                    TrimWavFile(reader, writer, startPos, endPos);
+                }
+            }
+        }
+
+        private static void TrimWavFile(WaveFileReader reader, WaveFileWriter writer, int startPos, int endPos)
+        {
+            // set blockalign -> 8 pour 24 bit
+            Int32 blockalign = reader.WaveFormat.BlockAlign;
+            Int32 bytealign = 0;
+            // 4 = 1024 pour 16bit, sinon 6 pour 24bit, 384 car c'est divisible par 6 sans faire planter le pc
+            if (blockalign == 4)
+                bytealign = 1024;
+            else
+                bytealign = 384;
+
+            reader.Position = startPos;
+            byte[] buffer = new byte[bytealign];
+            while (reader.Position < endPos)
+            {
+                int bytesRequired = (int)(endPos - reader.Position);
+                if (bytesRequired > 0)
+                {
+                    int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                    int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                    if (bytesRead > 0)
+                    {
+                        // WriteData obsolète
+                        writer.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+}
